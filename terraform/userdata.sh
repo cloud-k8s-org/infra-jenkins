3455178544737c892321a047bf32f698ac63aaf6
Gokul <gokulshyam567@gmail.com>
2024-06-01 20:59:56 -0400

    feat: replace nginx with caddy

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 
-sudo apt-get install -y nginx certbot python3-certbot-nginx
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy
 
-sudo ufw allow 'Nginx Full'
-sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/caddy/Caddyfile
 
-cat <<EOF > /etc/nginx/sites-available/jenkins
-server {
-  listen 80;
-  server_name ${JENKINS_DOMAIN_NAME};
-
-  location / {
-    proxy_pass http://localhost:8080;
-    proxy_set_header   Host \$host;
-    proxy_set_header   X-Real-IP \$remote_addr;
-    proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;
-    proxy_set_header   X-Forwarded-Proto \$scheme;
+cat <<EOF > /etc/caddy/Caddyfile
+${JENKINS_DOMAIN_NAME} {
+  reverse_proxy localhost:8080 {
+    header_up Host {host}
+    header_up X-Real-IP {remote}
+    header_up X-Forwarded-For {remote}
+    header_up X-Forwarded-Proto {scheme}
   }
 }
 EOF
 
-sudo ln -s /etc/nginx/sites-available/jenkins /etc/nginx/sites-enabled/
-sudo systemctl restart nginx
-
-# Obtain SSL certificate
-# Use --staging flag for testing to avoid rate limits
-sudo certbot --nginx -d ${JENKINS_DOMAIN_NAME} --non-interactive --agree-tos --email ${CERTBOT_EMAIL} --redirect
+sudo systemctl restart caddy
 
 sudo systemctl restart jenkins
 sudo systemctl status jenkins
